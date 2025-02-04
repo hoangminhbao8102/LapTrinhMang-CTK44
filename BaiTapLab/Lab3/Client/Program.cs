836f2d762bfcc83f8bf5b89cd9a006bba280d871
Minh Bảo Github <90952548+hoangminhbao8102@users.noreply.github.com>
2025-02-04 09:19:54 +0700

    Hoàn thành bài Lab 3

---
+++ BaiTapLab/Lab3/Client/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            RetryUdpClient client = new RetryUdpClient("127.0.0.1", 5000);
+            client.Run();
+        }
+    }
+
+    class RetryUdpClient
+    {
+        private Socket clientSocket;
+        private EndPoint serverEndPoint;
+        private byte[] data;
+
+        public RetryUdpClient(string serverIP, int serverPort)
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
+            clientSocket.Connect(serverEndPoint);
+            data = new byte[1024];
+
+            // Thiết lập timeout
+            int sockopt = (int)clientSocket.GetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout);
+            Console.WriteLine("Giá trị timeout mặc định: {0}", sockopt);
+            clientSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 3000);
+            sockopt = (int)clientSocket.GetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout);
+            Console.WriteLine("Giá trị timeout mới: {0}", sockopt);
+        }
+
+        private int SndRcvData(Socket s, byte[] message, EndPoint rmtdevice)
+        {
+            int recv;
+            int retry = 0;
+            while (true)
+            {
+                Console.WriteLine("Truyền lại lần thứ: #{0}", retry);
+                try
+                {
+                    s.SendTo(message, message.Length, SocketFlags.None, rmtdevice);
+                    data = new byte[1024];
+                    recv = s.ReceiveFrom(data, ref rmtdevice);
+                }
+                catch (SocketException)
+                {
+                    recv = 0;
+                }
+                if (recv > 0)
+                {
+                    return recv;
+                }
+                else
+                {
+                    retry++;
+                    if (retry > 4)
+                    {
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        public void Run()
+        {
+            string input, stringData;
+            int recv;
+            string welcome = "Hello Server";
+            recv = SndRcvData(clientSocket, Encoding.ASCII.GetBytes(welcome), serverEndPoint);
+            if (recv > 0)
+            {
+                stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine(stringData);
+            }
+            else
+            {
+                Console.WriteLine("Không thể liên lạc với thiết bị ở xa");
+                return;
+            }
+            while (true)
+            {
+                Console.Write("Nhập thông điệp gửi tới server: ");
+                input = Console.ReadLine();
+                if (input.Trim().ToLower() == "exit" || input.Trim().ToLower() == "exit all")
+                {
+                    SndRcvData(clientSocket, Encoding.ASCII.GetBytes(input), serverEndPoint);
+                    break;
+                }
+                recv = SndRcvData(clientSocket, Encoding.ASCII.GetBytes(input), serverEndPoint);
+                if (recv > 0)
+                {
+                    stringData = Encoding.ASCII.GetString(data, 0, recv);
+                    Console.WriteLine(stringData);
+                }
+                else
+                {
+                    Console.WriteLine("Không nhận được câu trả lời");
+                }
+            }
+            Console.WriteLine("Đang đóng client");
+            clientSocket.Close();
+        }
+    }
+}
---
+++ BaiTapLab/Lab3/Server/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            serverSocket.Bind(serverEndPoint);
+            Console.WriteLine("Đang chờ client kết nối tới...");
+
+            byte[] buff = new byte[1024];
+            EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            string str;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                int byteReceive = serverSocket.ReceiveFrom(buff, ref remoteEndPoint);
+                str = Encoding.ASCII.GetString(buff, 0, byteReceive);
+                Console.WriteLine("Nhận được từ phía client: " + str);
+            }
+
+            while (true)
+            {
+                // Nhận dữ liệu từ client
+                int byteReceive = serverSocket.ReceiveFrom(buff, ref remoteEndPoint);
+                str = Encoding.ASCII.GetString(buff, 0, byteReceive);
+                Console.WriteLine("Client: " + str);
+
+                if (str.Trim().ToLower() == "exit")
+                {
+                    Console.WriteLine("Client yêu cầu đóng kết nối.");
+                    break;
+                }
+                else if (str.Trim().ToLower() == "exit all")
+                {
+                    Console.WriteLine("Client yêu cầu đóng cả client và server.");
+                    serverSocket.Close();
+                    Console.WriteLine("Đã đóng server");
+                    Environment.Exit(0); // Đóng ứng dụng server
+                }
+
+                // Gửi phản hồi lại cho client
+                serverSocket.SendTo(buff, 0, byteReceive, SocketFlags.None, remoteEndPoint);
+
+                // Xử lý nhập từ người dùng trên server
+                string input = Console.ReadLine();
+                if (input == "exit")
+                    break;
+
+                serverSocket.SendTo(Encoding.ASCII.GetBytes(input), remoteEndPoint);
+
+                byte[] data = new byte[10];
+                int recv;
+                try
+                {
+                    recv = serverSocket.ReceiveFrom(data, ref remoteEndPoint);
+                    string stringData = Encoding.ASCII.GetString(data, 0, recv);
+                    Console.WriteLine(stringData);
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Cảnh báo: Dữ liệu bị mất, hãy thử lại.");
+                    data = new byte[data.Length + 10];
+                }
+            }
+
+            Console.WriteLine("Đã đóng kết nối với client");
+        }
+    }
+}
